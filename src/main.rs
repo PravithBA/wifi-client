2073456e796e23002a7d252bf851223ab463954e
Pravith <pravith10@gmail.com>
2023-03-03 00:28:01 +0530

    Features: 1. Added rust lol

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::process::Command;
+
+fn main() {
+
+//    let output = Command::new("nmcli").arg("dev").arg("wifi").arg("list").output().expect("Please install nmcli to make this work");
+//    let output_string = String::from_utf8_lossy(&output.stdout);
+//    for line in output_string.split("/n"){
+////        let words = line.split("  ");
+////        let mut is_start = false;
+////        let mut is_end = false;
+//        let mut d = 0;
+//        let mut space_count = 0;
+//        for letter in line.split(""){
+//            if letter == "D"{
+//                d += 1;
+//            }
+//            if d == 2 && letter == " "{
+//                space_count += 1;
+//            }
+//            if letter == "M"{
+//                d += 1;
+//            }
+//            println!("{}",letter)
+//        };
+//        println!("{}",space_count)
+////        for word in words {
+////            println!("{}",word);
+////        }
+//    }
+
+}
