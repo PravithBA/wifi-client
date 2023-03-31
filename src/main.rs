4cc27c7f3333d6443a70c23863340e9d4a500182
Pravith <pravith10@gmail.com>
2023-04-01 00:18:00 +0530

    Features: 1. Now it lists all the available SSIDs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
-use std::process::Command;
-use inquire::Select;
 use inquire::InquireError;
-
+use inquire::Select;
+use std::process::Command;
 
 fn main() {
-
     println!("Scanning for wifi...");
-    let output  = Command::new("nmcli").arg("-f").arg("SSID").arg("device").arg("wifi")
-        .output().expect("`nmcli` returned an error while scanning for wifi networks");
+    let output = Command::new("nmcli")
+        .arg("-t")
+        .arg("-f")
+        .arg("SSID")
+        .arg("device")
+        .arg("wifi")
+        .arg("list")
+        .output()
+        .expect("`nmcli` returned an error while scanning for wifi networks");
     let output_string = String::from_utf8_lossy(&output.stdout).to_string();
-    let mut wifi_ssids: Vec<String> = output_string.clone().split("\n").map(|s| s.to_string()).collect();
-    wifi_ssids.remove(0);
-    wifi_ssids.remove(wifi_ssids.len() - 1);
+    let wifi_ssids: Vec<String> = output_string
+        .split("\n")
+        .map(|s| s.to_string())
+        .collect();
     let wifi_ssid_options: Vec<&str> = wifi_ssids.iter().map(|s| &**s).collect();
-    let answer: Result<&str, InquireError> = Select::new("Select the SSID you want to connect", wifi_ssid_options).prompt();
+    let answer: Result<&str, InquireError> =
+        Select::new("Select the SSID you want to connect", wifi_ssid_options).prompt();
     let selected_ssid: &str;
     match answer {
         Ok(choice) => selected_ssid = choice,
         Err(_) => panic!("There was an error, please try again"),
     }
-    println!("Your selected SSID is: {}", selected_ssid);
-
+    println!("Your selected SSID is: `{}`", selected_ssid);
 }
